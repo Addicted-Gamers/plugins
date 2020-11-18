a444b84e929a561f32f6cf0fcb8f14463b1c7935
OrlandoAllen <40251392+OrlandoAllen@users.noreply.github.com>
2020-11-18 00:46:11 -0500

    Update webview_flutter_test.dart

--- packages/webview_flutter/test/webview_flutter_test.dart
+++ packages/webview_flutter/test/webview_flutter_test.dart
@@ -889,6 +889,22 @@ void main() {
 
     expect(platformWebView.userAgent, 'UA');
   });
+  testWidgets('onUpdateVisitedHistory', (WidgetTester tester) async {
+    String visitedUrl;
+
+    await tester.pumpWidget(WebView(
+        initialUrl: 'https://youtube.com',
+        onUpdateVisitedHistory: (String url) {
+          visitedUrl = url;
+        }));
+
+    final FakePlatformWebView platformWebView =
+        fakePlatformViewsController.lastCreatedView;
+
+    platformWebView.fakeOnUpdateVisitedHistoryCallback('https://google.com');
+
+    expect(visitedUrl, 'https://google.com');
+  });
 }
 
 class FakePlatformWebView {
@@ -1052,6 +1068,24 @@ class FakePlatformWebView {
     );
   }
 
+  void fakeOnUpdateVisitedHistoryCallback(String nextUrl) {
+    final StandardMethodCodec codec = const StandardMethodCodec();
+
+    final ByteData data = codec.encodeMethodCall(MethodCall(
+      'onUpdateVisitedHistory',
+      <dynamic, dynamic>{'url': nextUrl},
+    ));
+
+    // TODO(hterkelsen): Remove this when defaultBinaryMessages is in stable.
+    // https://github.com/flutter/flutter/issues/33446
+    // ignore: deprecated_member_use
+    BinaryMessages.handlePlatformMessage(
+      channel.name,
+      data,
+      (ByteData data) {},
+    );
+  }
+  
   void _loadUrl(String url) {
     history = history.sublist(0, currentPosition + 1);
     history.add(url);
